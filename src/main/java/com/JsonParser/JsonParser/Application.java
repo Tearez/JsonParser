4d615445b5698490fe67a3443c54625e8fc01aca
Tearez <martinreff1@gmail.com>
2018-12-02 18:55:45 +0200

    The app is runnable from .\JsonParser\build\libs\JsonParser-1.0.jar    using: java -jar {.\JsonParser\build\libs\JsonParser-1.0.jar} --input{input file name} --output{output file name}

---
+++ src/main/java/com/JsonParser/JsonParser/Application.java
@@ -0,0 +1,7 @@
+package com.JsonParser.JsonParser;
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/JsonParser/JsonParser/Parser/Parser.java
@@ -0,0 +1,4 @@
+package com.JsonParser.JsonParser.Parser;
+
+public class Parser {
+}
